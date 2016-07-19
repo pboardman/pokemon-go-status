30dd18aa833fccde39b678c221b0812a053ea22f
lacsap- <pascalboardman@gmail.com>
2016-07-19 18:24:15 -0400

    Fixed auto refresh not working

--- pokegostatus.py
+++ pokegostatus.py
@@ -3,9 +3,25 @@
 from bs4 import BeautifulSoup
 import rumps
 import requests
+import os
 
 
 class pokegostatus(rumps.App):
+
+    icons = ""
+    config_file = "{0}/.pokecheck".format(os.path.expanduser("~"))
+    if not os.path.isfile(config_file):
+        with open(config_file, "w") as f:
+            f.write("black\n")
+        icons = "b"
+    else:
+        with open(config_file, "r") as f:
+            content = f.read()
+            if "black" in content:
+                icons = "b"
+            elif "white" in content:
+                icons = "w"
+
 
     @rumps.clicked("Manual refresh")
     def manual_refresh(self, _):
@@ -13,14 +29,20 @@ class pokegostatus(rumps.App):
 
     @rumps.clicked("Black icons")
     def black_icons(self, _):
-        self.refresh(self, "b")
+        with open(self.config_file, "w") as f:
+            f.write("black\n")
+            self.icons = "b"
+        self.refresh(self)
 
     @rumps.clicked("White icons")
     def black_icons(self, _):
-        self.refresh(self, "w")
+        with open(self.config_file, "w") as f:
+            f.write("white\n")
+            self.icons = "w"
+        self.refresh(self)
 
     @rumps.timer(10)
-    def refresh(self, _, icons="b"):
+    def refresh(self, _):
         URL = "http://cmmcd.com/PokemonGo/"
 
         try:
@@ -31,13 +53,13 @@ class pokegostatus(rumps.App):
             status = False
 
         if status == 'Online!':
-            self.icon = "icons/%s/pokeok.png" % icons
+            self.icon = "icons/%s/pokeok.png" % self.icons
             self.title = None
         elif status == 'Unstable!':
-            self.icon = "icons/%s/pokeunstable.png" % icons
+            self.icon = "icons/%s/pokeunstable.png" % self.icons
             self.title = None
         elif status == 'Offline!':
-            self.icon = "icons/%s/pokedown.png" % icons
+            self.icon = "icons/%s/pokedown.png" % self.icons
             self.title = None
         elif not status:
             self.icon = None
